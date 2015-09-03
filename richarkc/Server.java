6314da0da6d2ecbf7713bc73d801b3e7c6fe43a9
richarkc <richarkc@muohio.edu>
2015-09-03 10:11:28 -0400

    Fully functioning

--- richarkc/Server.java
+++ richarkc/Server.java
@@ -1,10 +1,12 @@
 /*
-   Scott Campbell
-   CSE383-f15
+   Kyle Richardson
+   CSE383 - Fall 2015
+   9/2/2015
 
-   Lab1
+   Server.java
 
-   Sample datagram server
+   Simple UDP server that waits for a client to send messages, and then echoes the messages back to all other clients.
+   Modified from code provided by Dr. Scott Campbell
 
  */
 
@@ -42,16 +44,14 @@ public class Server {
 	public Server(int port) throws IOException {
 		this.port = port;
 		sock = new DatagramSocket(port);
-		log = new Log("UDP Server.log");
+		log = new Log("server.log");
 	}
 
-	//main working code for server
 	/*
-	Listens for message from client and sends them the previous message received
+	Waits for packet from clients
 	*/
 	public void Main() {
-		String msg = "first";
-		System.out.println("Server Starting");	//only message I will send to std out
+		System.out.println("Server Starting");	
 		log.log("Server Starting");
 		clientList = new ArrayList<SocketAddress>();
 
@@ -65,38 +65,70 @@ public class Server {
 				DatagramPacket pkt= new DatagramPacket(b,b.length);
 				sock.receive(pkt);
 
-				log.log(pkt.getSocketAddress() + " Got Packet" );
 				ByteArrayInputStream bis= new ByteArrayInputStream(b);
 				DataInputStream dis = new DataInputStream(bis);
 				String newmsg = dis.readUTF();
-				System.out.println("Got a packet from " + pkt.getSocketAddress());
-				System.out.println(newmsg);
-				
-				if (newmsg.startsWith("HELLO")) {
-					if (!(clientList.contains(pkt.getSocketAddress()))) {
-						clientList.add(pkt.getSocketAddress());
-						sendMessage("HELLO-RESPONSE","", pkt.getSocketAddress());
-					}
-				
-						
+				//Decipher message using protocol	
+				try{
+					readProtocol(newmsg, dis.readUTF(), pkt.getSocketAddress());
 				}
-				
-				else if (newmsg.startsWith("MESSAGE")) {
-					sendAll(dis.readUTF());
+				catch (IllegalArgumentException e) {
+					//if the client provides an incorrect data type or the incorrect amount of arguments this will catch it
+					log.log("Invalid command from: " + pkt.getSocketAddress());
+					sendMessage("MESSAGE", "Invalid command, try again", pkt.getSocketAddress());
 				}
 
-				
-				else if (newmsg.startsWith("GOODBYE")){
-					sendMessage("GOODBYE-RESPONSE","",pkt.getSocketAddress());
-					clientList.remove(pkt.getSocketAddress());
-				}
-					
 
-			} catch (IOException err) {
+			} catch (Exception err) {
 				log.log("Error " + err);
 			}
 		}
 	}
+	/*
+	readProtocol takes in the messages from the client and translates it into valid statement in the following protocol:
+	HELLO: Adds a client to the connected list and allows for them to receive echoed messsages, server sends back "HELLO-RESPONSE"
+	MESSAGE: Receives a message from the client and echoes it to all connected clients
+	GOODBYE: Removes a client from the connected list, server sends back "GOODBYE-RESPONSE"
+	*/	
+	public void readProtocol(String newmsg, String msg2, SocketAddress add) throws IOException {
+		
+		if (newmsg.startsWith("HELLO")) {
+			if (!(clientList.contains(add))) {
+				clientList.add(add);
+				sendMessage("HELLO-RESPONSE","", add);
+				log.log("Client connected: " + add);
+				}
+									
+			}
+				
+			else if (newmsg.startsWith("MESSAGE")) {
+				if (msg2.length() > 500) {
+					log.log("Message from " + add + " too long, declined");
+					sendMessage("MESSAGE", "ERROR: Messages are limited to 500 characters", add);
+				}
+				else {
+					log.log("Received message from: " + add + " " + msg2);
+					sendAll(msg2);
+				}
+			}
+
+				
+			else if (newmsg.startsWith("GOODBYE")){
+				sendMessage("GOODBYE-RESPONSE","", add);
+				clientList.remove(add);
+				log.log("Client disconnected: " + add);
+			}
+				
+			else {
+				log.log("Invalid command from " + add + ": " + msg2);
+				sendMessage("MESSAGE", "Invalid command, try again", add);
+			}
+
+	}
+
+	/*
+	sendMessage send a message to one client, typically used for "HELLO-RESPONSE" and "GOODBYE-RESPONSE" and for errors
+	*/
 
 	public void sendMessage(String msg, String msg2, SocketAddress add) throws IOException {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -108,15 +140,21 @@ public class Server {
 		sock.send(sendResponse);
 	}
 
+	/*
+	sendAll will send a message to all connected clients
+	*/
+
 	public void sendAll(String msg) throws IOException {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		DataOutputStream dos = new DataOutputStream(bos);
 		dos.writeUTF("MESSAGE");
 		dos.writeUTF(msg);
 		byte data[] = bos.toByteArray();
+		//loop to send to all clients in clientList
 		for (SocketAddress i : clientList) {
 			DatagramPacket sendResponse = new DatagramPacket(data, data.length, i);	
 			sock.send(sendResponse);
+			log.log("Sent message to " + i.toString() + " " + msg);
 		}	
 
 	}
